45d98acd29c274f682f7353eb6704a781147b476
Md Arman <123488184+Armansonu007@users.noreply.github.com>
2023-06-23 23:04:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ScenicView.css";
+
+const ScenicView = () => {
+  return (
+    <div className="scenic-view">
+      <div className="deer"></div>
+      {/* Add more elements and animations */}
+    </div>
+  );
+};
+
+export default ScenicView;
